d0816f37b4e5bec53221ecd1f2e3021eae2fbfc8
chenjianjun <chenjianjun@cvte.com>
2019-03-13 18:46:19 +0800

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_cjj_mp3encoder_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
